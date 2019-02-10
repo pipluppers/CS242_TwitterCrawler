1376f3380f1b6ec95058c05aca79da0737006f6b
Alex Nguyen <nguyenalex24@gmail.com>
2019-02-09 17:53:06 -0800

    Uploaded crawler batch script

---
+++ crawler.bat
@@ -0,0 +1,8 @@
+:: Disables displaying the command
+@echo off
+
+:: Clear the terminal
+cls
+
+:: input.txt will be the input file containing a single string denoting the screen name of the user the crawler will start from
+python pips.py output.txt
